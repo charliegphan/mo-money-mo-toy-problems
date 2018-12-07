d0ed3016645771a1e1fe18e8ebbf8b14bc0c0741
Charlie Phan <charliegphan@gmail.com>
2018-12-07 14:42:56 -0800

    tentative asyncMap

--- asyncMap/asyncMap.js
+++ asyncMap/asyncMap.js
@@ -38,6 +38,41 @@
  *
  */
 
-
 var asyncMap = function(tasks, callback) {
+  const resultsArray = [];
+  let resultsCount = 0;
+
+  for (var i = 0; i < tasks.length; i++) {
+    // (function (index) {
+      console.log(i);
+      var cb = function(result) {
+        resultsArray[i] = result;
+        resultsCount++;
+  
+        if (resultsCount === tasks.length) {
+          callback(resultsArray);
+        }
+      }
+  
+      tasks[i](cb);
+    // })(i);
+  }
 };
+
+ asyncMap([
+  function(cb){
+    setTimeout(function(){
+      cb('one');
+    }, 200);
+  },
+  function(cb){
+    setTimeout(function(){
+      cb('two');
+    }, 100);
+  }
+ ],
+  function(results){
+    // the results array will equal ['one','two'] even though
+    // the second function had a shorter timeout.
+    console.log(results); // ['one', 'two']
+ });
