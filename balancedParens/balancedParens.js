da7880ab878a9888c596442e8cfea1a0d9d086a9
Charlie Phan <charliegphan@gmail.com>
2018-12-07 16:27:06 -0800

    complete balancedParens

--- balancedParens/balancedParens.js
+++ balancedParens/balancedParens.js
@@ -24,6 +24,28 @@
  *
  */
 var balancedParens = function(input) {
+  const stack = [];
+  const pairs = {'(':')','{':'}','[':']'};
+
+  for (let i = 0; i < input.length; i++) {
+    let char = input[i];
+
+    if (pairs[char]) {
+      stack.push(char);
+    } else if (char === ')' || char === '}' || char === ']') {
+      if (pairs[stack.pop()] !== char) {
+        return false;
+      }
+    }
+  };
+
+  return stack.length === 0;
 };
 
 
+console.log(balancedParens('[](){}')); // true
+console.log(balancedParens('[({})]'));   // true
+console.log(balancedParens('[(]{)}')); // false
+
+console.log(balancedParens(' var wow  = { yo: thisIsAwesome() }')); // true
+console.log(balancedParens(' var hubble = function() { telescopes.awesome();')); // false
